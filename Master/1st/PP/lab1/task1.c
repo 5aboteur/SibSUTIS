8f8a28535c85867b3bdf79324b8abe6993939c15
Roman <rtereshkov@gmail.com>
2016-10-13 00:02:56 +0700

    Update task1.c

--- Master/1st/PP/lab1/task1.c
+++ Master/1st/PP/lab1/task1.c
@@ -54,9 +54,9 @@ void saxpy_sse(float * __restrict x, float * __restrict y, float a, int n)
 	__m128 *xx = (__m128 *)x;
 	__m128 *yy = (__m128 *)y;
 
-	int k = n / 4;							// float -> 32bit
+	int k = n / 4;					// float -> 32bit
 
-	__m128 aa = _mm_set1_ps(a);				// [a, a, a, a]
+	__m128 aa = _mm_set1_ps(a);			// [a, a, a, a]
 
 	for (int i = 0; i < k; ++i) {
 		__m128 z = _mm_mul_ps(aa, xx[i]);	// [aa0 * xx0, ..]
@@ -64,10 +64,8 @@ void saxpy_sse(float * __restrict x, float * __restrict y, float a, int n)
 	}
 
 	/* Loop reminder (n % 4 != 0) ? */
-	if (n % 4) {
-		for (int i = k * 4; i < n; ++i)
-			y[i] = a * x[i] + y[i];
-	}
+	for (int i = k * 4; i < n; ++i)
+		y[i] = a * x[i] + y[i];
 }
 
 void daxpy(double *x, double *y, double a, int n)
@@ -81,7 +79,7 @@ void daxpy_sse(double * __restrict x, double * __restrict y, double a, int n)
 	__m128d *xx = (__m128d *)x;
 	__m128d *yy = (__m128d *)y;
 
-	int k = n / 2;							// double -> 64bit
+	int k = n / 2;					// double -> 64bit
 
 	__m128d aa = _mm_set1_pd(a);			// [a, a, a, a]
 
@@ -91,10 +89,8 @@ void daxpy_sse(double * __restrict x, double * __restrict y, double a, int n)
 	}
 
 	/* Loop reminder (n % 4 != 0) ? */
-	if (n % 2) {
-		for (int i = k * 2; i < n; ++i)
-			y[i] = a * x[i] + y[i];
-	}
+	for (int i = k * 2; i < n; ++i)
+		y[i] = a * x[i] + y[i];
 }
 
 void *xmalloc(size_t size)
