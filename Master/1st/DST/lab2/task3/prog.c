ed374e0a34a17b8ffb5dcd830516f5d4f3ca5519
Roman <rtereshkov@gmail.com>
2016-12-19 15:20:55 +0700

    Update prog.c

--- Master/1st/DST/lab2/task3/prog.c
+++ Master/1st/DST/lab2/task3/prog.c
@@ -3,23 +3,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <xmmintrin.h>
 
-#define SIZE 1024
+#define SIZE 8
 
 void foo(float *v1, float *v2)
 {
 	for (int i = 0; i < SIZE; ++i) {
-		v1[i] = (float)rand() / (float)RAND_MAX;
-		v2[i] = (float)rand() / (float)RAND_MAX;
+//		v1[i] = (float)rand() / (float)RAND_MAX;
+//		v2[i] = (float)rand() / (float)RAND_MAX;
+		v1[i] = 2;
+		v2[i] = 4;
+
 	}
 }
 
 void bar(float *v1, float *v2, float *v3)
 {
-	for (int i = 0; i < SIZE; ++i)
-		v3[i] = sqrtf(v1[i] * v2[i]);
+	__m128 *vv1 = (__m128 *)v1, *vv2 = (__m128 *)v2,
+			*vv3 = (__m128 *)v3;
+
+	for (int i = 0; i < SIZE / 4; ++i) {
+		__m128 t1 = _mm_mul_ps(vv1[i], vv2[i]); 
+		vv3[i] = _mm_sqrt_ps(t1);
+	}
+//		v3[i] = sqrtf(v1[i] * v2[i]);
 }
-/*
+
 void print(float *v1, float *v2, float *v3)
 {
 	printf("\033[1;32mVec1:\n*****\n\033[0m");
@@ -46,7 +56,7 @@ void print(float *v1, float *v2, float *v3)
 	}
 	printf("\n");
 }
-*/
+
 int main()
 {
 	srand((unsigned)time(NULL));
@@ -56,7 +66,7 @@ int main()
 	foo(vec1, vec2);
 	bar(vec1, vec2, vec3);
 
-//	print(vec1, vec2, vec3);
+	print(vec1, vec2, vec3);
 
 	return 0;
 }
