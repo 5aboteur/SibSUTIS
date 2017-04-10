ed9ee865df0f1b7970e69fdaf5244ee675c3f7db
Roman <rtereshkov@gmail.com>
2017-04-11 00:33:42 +0700

    Add files via upload

--- Master/1st/MPCS/lab1/prog.c
+++ Master/1st/MPCS/lab1/prog.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 int main()
 {
 	srand((unsigned)time(NULL));
 
-	char alphabet[10] = "zxcvbyouai";
+	char alphabet[10] = "zyxocuvabi";
 	char vowel[5] = "youai";
 	char consonant[5] = "zxcvb";
 
-	char buf1[11], buf2[11], buf3[11], buf4[11];
+	char buf1[10], buf2[10], buf3[10], buf4[10];
 
-	int i, p, p1, p2, p3, p4;
+	memset(buf1, 0, sizeof(char) + 10);
+	memset(buf2, 0, sizeof(char) + 10);
+	memset(buf3, 0, sizeof(char) + 10);
+	memset(buf4, 0, sizeof(char) + 10);
+
+	int i, p, p1, p2, p3, p4, cnt2 = 0, cnt3 = 0;
 	p = p1 = p2 = p3 = p4 = 0;
 
 	/***** routine *****/
 	while (0x1) {
-
-//		printf("| %2d | %2d | %2d | %2d | %2d |\n",
-//			p, p1, p2, p3, p4);
 
 		do {
 			p = 1 + rand() % 4;
@@ -34,7 +37,7 @@ int main()
 
 		switch (p) {
 		case 1:
-			// all
+			// random
 			buf1[p1] = alphabet[p1];
 			p1++;
 			break;
@@ -42,7 +45,7 @@ int main()
 			// vowel
 			for (i = 0; i < 5; i++) {
 				if (alphabet[p2] == vowel[i]) {
-					buf2[i] = alphabet[p2];
+					buf2[cnt2++] = alphabet[p2];
 					break;
 				}
 			}
@@ -52,7 +55,7 @@ int main()
 			// consonant
 			for (i = 0; i < 5; i++) {
 				if (alphabet[p3] == consonant[i]) {
-					buf3[i] = alphabet[p3];
+					buf3[cnt3++] = alphabet[p3];
 					break;
 				}
 			}
@@ -60,25 +63,19 @@ int main()
 			break;
 		case 4:
 			// asterisk
-			buf4[p4] = '*';
+			alphabet[p4] = '*';
 			p4++;
 			break;
 		}
 
-		// check if done
-		if ((p1 == 10) && (p2 == 10) && (p3 == 10) && (p4 == 10)) {
-			buf1[10] = '\0';
-			buf2[5]  = '\0';
-			buf3[5]  = '\0';
-			buf4[10] = '\0';
+		if ((p1 == 10) && (p2 == 10) && (p3 == 10) && (p4 == 10))
 			break;
-		}
 
 	}
 
 	printf("p1: %d\np2: %d\np3: %d\np4: %d\n\n", p1, p2, p3, p4);
-	printf("buf1: %s\nbuf2: %s\nbuf3: %s\nbuf4: %s\n\n",
-		buf1, buf2, buf3, buf4);
+	printf("buf1: %s\nbuf2: %s\nbuf3: %s\n\n",
+		buf1, buf2, buf3);
 	printf("Elapsed time: %.6f ms.\n",
 		(float)clock() / CLOCKS_PER_SEC * 1000.0f);
 
